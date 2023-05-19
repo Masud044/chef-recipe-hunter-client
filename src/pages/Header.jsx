06dc581d7e602fdbae620780b6677a141dca326c
Masud <ranamasud0701@gmail.com>
2023-05-20 00:20:53 +0600

    added

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -10,16 +10,16 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <Link to='/'> <li className='text-white' ><a>Home</a></li></Link>  
+                    <Link to='/'> <li className='' > <a>Home</a></li></Link>  
                         
-                        <li ><a>Blog</a></li>
+                        <li >Blog</li>
                     </ul>
                 </div>
                 <li className="btn btn-ghost normal-case text-xl text-white no-underline">Flavorsome Kitchen</li>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                <Link to='/'> <li className='text-white' ><a>Home</a></li></Link>   
+                <Link to='/'> <li className='text-white' > <a>Home</a></li></Link>   
                     
                     <li className='text-white'><a>Blog</a></li>
                 </ul>
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,9 +1,12 @@
+
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+
 
 const RecipeDetails = () => {
     const { id } = useParams();
     const [recipe, setRecipe] = useState([]);
+    const [disable,setDisable] = useState(false);
     //console.log(id)
     useEffect(() => {
         fetch(`http://localhost:5000/recipe/${id}`)
@@ -11,49 +14,81 @@ const RecipeDetails = () => {
             .then(data => setRecipe(data))
     }, [])
 
-    const { chef_name, chef_picture, ingredients, recipe_name, instructions, num_recipes, years_of_experience, rating } = recipe;
+    const { chef_name, chef_picture, ingredients, recipe_name1,recipe_name2,recipe_name3, instructions, num_recipes, years_of_experience, rating } = recipe;
 
 
-    //console.log(recipe_details.recipe_name)
+    //console.log(recipe_name)
+    const handleclick=(event)=>{
+         alert('add favorite recipe');
+        
+         //console.log(event.currentTarget)
+         setDisable(true);
+    }
+   
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto mt-2'>
             <div className="card lg:card-side bg-base-100 shadow-xl">
                 <figure><img src={chef_picture} alt="Album" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{chef_name}</h2>
 
-                    <ul className='list-disc text-lime-800'>
-
-                        <li>  {recipe_name}</li>
-
-                    </ul>
+                   
                     <p className='mb-0'> <span className=' text-lime-800'>Years of experience:</span>  {years_of_experience}</p>
                     <p className='lg:-mt-20'><span className='text-lime-800'>Numbers of recipes</span> : {num_recipes}</p>
                     <p className='lg:-mt-20'> <span className='text-lime-900 font-medium'>Likes</span> :123344</p>
-                    <p className='lg:-mt-20'> <span className='text-lime-900 font-medium'>Rating</span> : {rating}</p>
-                    <div className="card-actions justify-end">
+                   
+                   
+                   
+                </div>
+            </div> 
 
+            <div className='grid lg:grid-cols-3 mt-10'>
+                <div className="card card-compact bg-base-100 shadow-xl">
+                    <figure><img src="https://i.ibb.co/wLxbBhC/images-2.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                    <p className=''> <span className='text-lime-900 font-medium'>Rating</span> : 4.9</p>
+                        <h2 className="card-title">Recipe name:{recipe_name1}</h2>
+                        
+                        <p className='mt-4'><span className='text-2xl'>cooking method:</span>{instructions}</p>
+                       {
+                        ingredients?.map((i,k) => <li key={k}>{i}</li>)
+                       }
+                        <button disabled ={disable} onClick={handleclick} className=" bg-lime-800 rounded-lg w-32 p-4 text-white">Add Favorite</button>
+                    </div>
+                </div>
+                <div className="card card-compact  bg-base-100 shadow-xl">
+                    <figure><img src="https://i.ibb.co/wLxbBhC/images-2.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                    <p className=''> <span className='text-lime-900 font-medium'>Rating</span> : 3.9</p>
+                        <h2 className="card-title">Recipe name:{recipe_name2}</h2>
+                        <p><span className='text-2xl'>cooking method:</span>{instructions}</p>
+                        {
+                        ingredients?.map((i,k) => <li key={k}>{i}</li>)
+                       }
+                        <button disabled ={disable} onClick={handleclick} className=" bg-lime-800 rounded-lg w-32 p-4 text-white">Add Favorite</button>
+                        
+                    </div>
+                </div>
+                <div className="card card-compact  bg-base-100 shadow-xl">
+                    <figure><img src="https://i.ibb.co/wLxbBhC/images-2.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                    <p className=''> <span className='text-lime-900 font-medium'>Rating</span> : 4.4</p>
+                        <h2 className="card-title">Recipe name:{recipe_name3}</h2>
+                        <p><span className='text-2xl'>cooking method:</span>{instructions}</p>
+                        {
+                        ingredients?.map((i,k) => <li key={k}>{i}</li>)
+                       }
+                      
+                           
+                         <button disabled ={disable} onClick={handleclick} className=" bg-lime-800 rounded-lg w-32 p-4 text-white">Add Favorite</button>
+                     
                        
-                        <button className="bg-lime-800 p-4 rounded-lg text-white">Favorite recipe</button>
                        
                     </div>
                 </div>
             </div>
 
 
-
-            <div className='mt-10'>
-
-                <li>{ingredients}</li>
-
-            </div>
-
-
-
-
-            <div className='mt-4 justify-evenly'>
-                <p><span className='text-2xl'>cooking method:</span>{instructions}</p>
-            </div>
         </div>
     );
 };
