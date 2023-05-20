20be40b67b3ba199442ca92f0edb5add248e2fe1
Masud <ranamasud0701@gmail.com>
2023-05-21 00:36:24 +0600

    loader add

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -8,23 +8,25 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const[loading,setLoading] = useState(true);
 
     const createUser = (email, password) => {
-        
+         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password);
     }
     const signIn = (email,password)=>{
+        setLoading(true)
          return signInWithEmailAndPassword(auth,email,password);
     }
     const logOut = () => {
-       
+        setLoading(true)
         return signOut(auth);
     }
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, loggedUser => {
             console.log('logged in user inside auth state observer', loggedUser)
             setUser(loggedUser);
-           // setLoading(false);
+            setLoading(false);
         })
 
         return () => {
@@ -33,6 +35,7 @@ const AuthProvider = ({ children }) => {
     }, [])
     const authInfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
